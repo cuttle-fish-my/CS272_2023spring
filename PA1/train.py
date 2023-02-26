357d3b283eb759181c5662941e4afaf9cd75a6e3
libn <libn@shanghaitech.edu.cn>
2023-02-26 22:41:01 +0800

    add modification for resnet

--- PA1/train.py
+++ PA1/train.py
@@ -4,16 +4,23 @@ import torchvision
 import utils
 import os
 
+dev = utils.dev()
+
 
 def train(opt):
     model = utils.create_model(opt)
+    model = model.to(device=dev)
     train_loader, val_loader = utils.creat_data_loader(opt, train=True)
+    optimizer = torch.optim.SGD(model.parameters(), lr=opt.lr, momentum=opt.momentum, weight_decay=opt.weight_decay)
     for epoch in range(opt.start_epoch, opt.epochs):
+        model.train()
         for data, label in train_loader:
-            print(data.shape)
-            print(label.shape)
-            break
-        break
+            data, label = data.to(device=dev), label.to(device=dev)
+            output = model(data)
+            loss = torch.nn.functional.cross_entropy(output, label)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
 
 if __name__ == "__main__":
--- PA1/utils.py
+++ PA1/utils.py
@@ -4,11 +4,12 @@ import torch
 import platform
 import argparse
 import torch.utils.data
-
+import torch.nn as nn
 import torchvision
 
 from torch.utils.data import DataLoader as DataLoader
 from torchvision.datasets import CIFAR10 as CIFAR10
+from torchvision.models import resnet
 
 
 def get_args(train: bool = True):
@@ -18,10 +19,8 @@ def get_args(train: bool = True):
     parser.add_argument('--model_name', type=str, default='resnet18')
     parser.add_argument('--model_dir', type=str, default='None')
     parser.add_argument('--imagenet_pretrained', type=bool, default=False)
+    parser.add_argument('--dataset_name', type=str, default='CIFAR10')
     if train:
-        parser.add_argument('--train_data', type=str, default='CIFAR10')
-        parser.add_argument('--val_data', type=str, default='CIFAR10')
-
         parser.add_argument('--start_epoch', type=int, default=0)
         parser.add_argument('--batch_size', type=int, default=1)
         parser.add_argument('--epochs', type=int, default=1000)
@@ -33,8 +32,6 @@ def get_args(train: bool = True):
         parser.add_argument('--log_interval', type=int, default=10)
         parser.add_argument('--save_interval', type=int, default=100)
         parser.add_argument('--save_dir', type=str, default='models')
-    else:
-        parser.add_argument('--test_data', type=str, default='CIFAR10')
 
     return parser.parse_args()
 
@@ -44,15 +41,21 @@ def create_model(opt):
     pretrained = opt.imagenet_pretrained
     model = None
     if model_name == 'resnet18':
-        model = torchvision.models.resnet18(pretrained=pretrained)
+        model = resnet.resnet18(weights=resnet.ResNet18_Weights if pretrained else None)
     elif model_name == 'resnet34':
-        model = torchvision.models.resnet34(pretrained=pretrained)
+        model = resnet.resnet34(weights=resnet.ResNet34_Weights if pretrained else None)
     elif model_name == 'resnet50':
-        model = torchvision.models.resnet50(pretrained=pretrained)
+        model = resnet.resnet50(weights=resnet.ResNet50_Weights if pretrained else None)
     elif model_name == 'resnet101':
-        model = torchvision.models.resnet101(pretrained=pretrained)
+        model = resnet.resnet101(weights=resnet.ResNet101_Weights if pretrained else None)
     elif model_name == 'resnet152':
-        model = torchvision.models.resnet152(pretrained=pretrained)
+        model = resnet.resnet152(weights=resnet.ResNet152_Weights if pretrained else None)
+
+    if opt.dataset_name == 'CIFAR10':
+        conv1_in_channels = model.conv1.in_channels
+        model.conv1 = resnet.conv3x3(3, conv1_in_channels)
+        fc_in_features = model.fc.in_features
+        model.fc = nn.Linear(fc_in_features, 10)
 
     if not pretrained and os.path.exists(opt.model_dir):
         model.load_state_dict(torch.load(opt.model_dir))
@@ -61,35 +64,25 @@ def create_model(opt):
 
 
 def creat_data_loader(opt, train):
-    if train:
-        if opt.train_data == 'CIFAR10':
-            transform = torchvision.transforms.Compose([
-                torchvision.transforms.RandomHorizontalFlip(),
-                torchvision.transforms.ToTensor(),
-                torchvision.transforms.Normalize(
-                    mean=[0.4914, 0.4822, 0.4465],
-                    std=[0.2023, 0.1994, 0.2010])
-            ])
-            dataset = CIFAR10(root='./data/CIFAR10', train=True, download=True, transform=transform)
+    if opt.dataset_name == 'CIFAR10':
+        transform = []
+        if train:
+            transform.append(torchvision.transforms.RandomHorizontalFlip())
+        transform.append(torchvision.transforms.ToTensor())
+        transform.append(torchvision.transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010]))
+        transform = torchvision.transforms.Compose(transform)
+        dataset = CIFAR10(root='./data/CIFAR10', train=train, download=True, transform=transform)
+        if train:
             train_dataset, val_dataset = torch.utils.data.random_split(dataset, [45000, 5000])
-            train_loader = DataLoader(dataset, batch_size=opt.batch_size, shuffle=True, pin_memory=True)
+            train_loader = DataLoader(train_dataset, batch_size=opt.batch_size, shuffle=True, pin_memory=True)
             val_loader = DataLoader(val_dataset, batch_size=opt.batch_size, shuffle=True, pin_memory=True)
             return train_loader, val_loader
         else:
-            pass
-    else:
-        if opt.test_data == 'CIFAR10':
-            transform = torchvision.transforms.Compose([
-                torchvision.transforms.ToTensor(),
-                torchvision.transforms.Normalize(
-                    mean=[0.4914, 0.4822, 0.4465],
-                    std=[0.2023, 0.1994, 0.2010])
-            ])
-            test_dataset = CIFAR10(root='./data/CIFAR10', train=False, download=True, transform=transform)
-            test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False, pin_memory=True)
+            test_loader = DataLoader(dataset, batch_size=1, shuffle=False, pin_memory=True)
             return test_loader
-        else:
-            pass
+    else:
+        print("ShanghaiTech_Crowd_Counting_Dataset not implemented yet!")
+        pass
 
 
 def dev():

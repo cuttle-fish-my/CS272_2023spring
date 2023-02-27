102007b4d74a2650b047f1f6b9723e8f3ef789b8
libn <libn@shanghaitech.edu.cn>
2023-02-27 14:17:56 +0800

    add test part

--- PA1/test.py
+++ PA1/test.py
@@ -1,5 +1,19 @@
 import torch
 import torchvision
 
-model = torchvision.models.resnet18(pretrained=True)
+import utils
 
+dev = utils.dev()
+
+
+def test(opt):
+    model, _ = utils.create_model(opt)
+    model = model.to(dev)
+    test_loader = utils.creat_data_loader(opt, train=False)
+    loss, acc, _ = utils.run_one_epoch(model, optimizer=None, loader=test_loader, train=False)
+    print("test loss = {:.2f}, test accuracy = {.2%}".format(loss, acc))
+
+
+if __name__ == '__main__':
+    args = utils.get_args(train=False)
+    test(args)

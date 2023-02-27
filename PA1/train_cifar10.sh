3b8c4422472eb886750c2a292c14f96a27da6aa7
libn <libn@shanghaitech.edu.cn>
2023-02-27 14:52:24 +0800

    add test part

--- PA1/train_cifar10.sh
+++ PA1/train_cifar10.sh
@@ -1,1 +1,1 @@
-python train.py --exp_name CIFAR10 --model_name resnet34 --batch_size 128 --save_interval 5 --epochs 200 --lr 0.1
+python train.py --exp_name CIFAR10 --model_name resnet34 --batch_size 128 --save_interval 5 --epochs 200 --lr 0.1 --load_dir models/CIFAR10/100

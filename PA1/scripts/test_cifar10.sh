5eb246f73626b5e96f9580547cf41d352de13ea4
libn <libn@shanghaitech.edu.cn>
2023-02-28 18:12:26 +0800

    optimized file structure

--- PA1/scripts/test_cifar10.sh
+++ PA1/scripts/test_cifar10.sh
@@ -1,4 +1,5 @@
+read -p "Enter the experiment name: " exp_name
 read -p "Enter the index of model to load: " load_index
 
-python ../test.py --exp_name CIFAR10 --model_name resnet34 --load_dir ../models/CIFAR10/${load_index} --dataset_dir ../data
+python ../test.py --exp_name ${exp_name} --model_name resnet34 --load_dir ../models/CIFAR10/${load_index} --dataset_dir ../data
 

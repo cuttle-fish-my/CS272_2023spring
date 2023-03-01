d660f9e080de8a7544c30f53dd16d6e6b18c6683
libn <libn@shanghaitech.edu.cn>
2023-03-02 01:29:58 +0800

    add CrowdCounting training part

--- PA1/scripts/train_CrowdCounting.sh
+++ PA1/scripts/train_CrowdCounting.sh
@@ -1,3 +1,3 @@
 read -p "Enter the index of model to load: " load_index
-python ../train.py --exp_name CrowdCounting --model_name CrowdCountingResnet --dataset_name CrowdCounting --batch_size 16 --save_interval 5 --epochs 200 --lr 0.1 --load_dir ../models/CrowdCounting/${load_index} --save_dir ../models --dataset_dir ../data
+python ../train.py --exp_name CrowdCounting --model_name CrowdCountingResnet --dataset_name CrowdCounting --batch_size 8 --save_interval 5 --epochs 200 --lr 0.1 --load_dir ../models/CrowdCounting/${load_index} --save_dir ../models --dataset_dir ../data
 

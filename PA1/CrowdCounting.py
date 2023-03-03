f34df414270a014cc8052311779703a2fa5af343
libn <libn@shanghaitech.edu.cn>
2023-03-04 01:10:23 +0800

    add CrowdCounting training part

--- PA1/CrowdCounting.py
+++ PA1/CrowdCounting.py
@@ -60,4 +60,4 @@ def CrowdCountingLoss(pred, gt):
     MAE = torch.mean(torch.abs(z_pred - z_label))
     # return MSE + MAE
     # return torch.square(pred - gt[:, None, :, :]).mean()
-    return torch.nn.BCELoss()(pred, gt[:, None, :, :]) + MSE * 1e-5
+    return torch.nn.BCELoss()(pred, gt[:, None, :, :]) + MSE * 1e-6 + MAE * 1e-6

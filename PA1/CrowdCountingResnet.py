e122083f75f0ae14df39cf5ee95d1edbc7d51794
libn <libn@shanghaitech.edu.cn>
2023-03-04 01:31:24 +0800

    add CrowdCounting training part

--- PA1/CrowdCountingResnet.py
+++ PA1/CrowdCountingResnet.py
@@ -11,17 +11,18 @@ class UpSamplingBlock(nn.Module):
         self.bn = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU(inplace=True)
         self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         identity = self.upsample(x)
         out = self.conv1(identity)
         out = self.bn(out)
-        out = self.relu(out)
+        out = self.sigmoid(out)
         out = self.conv2(out)
         out = self.bn(out)
         # identity = self.conv1x1(identity)
         # out += identity
-        out = self.relu(out)
+        out = self.sigmoid(out)
         return out
 
 
@@ -39,7 +40,7 @@ class CrowdCountingResnet(resnet.ResNet):
     def _forward_impl(self, x):
         x = self.conv1(x)
         x = self.bn1(x)
-        x = self.relu(x)
+        x = self.sigmoid(x)
         x = self.maxpool(x)
 
         x = self.layer1(x)

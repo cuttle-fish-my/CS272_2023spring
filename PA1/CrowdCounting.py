94527b79c790010f2e3f7db88a946ae1fe80539c
libn <libn@shanghaitech.edu.cn>
2023-03-04 16:58:45 +0800

    add CrowdCounting training part

--- PA1/CrowdCounting.py
+++ PA1/CrowdCounting.py
@@ -52,11 +52,11 @@ def CrowdCountingTransform(img, gt, train: bool = True):
 
 
 def CrowdCountingLoss(pred, gt):
-    return torch.square(pred - gt[:, None, :, :]).sum() / (2 * pred.shape[0])
-    # z_pred = pred[:, 0, :, :].sum(axis=(1, 2))
-    # z_label = dilated_gt.sum(axis=(1, 2))
-    # MSE = torch.sqrt(torch.mean((z_pred - z_label) ** 2))
-    # MAE = torch.mean(torch.abs(z_pred - z_label))
+    z_pred = pred[:, 0, :, :].sum(axis=(1, 2))
+    z_label = gt.sum(axis=(1, 2))
+    MSE = torch.sqrt(torch.mean((z_pred - z_label) ** 2))
+    MAE = torch.mean(torch.abs(z_pred - z_label))
+    return torch.square(pred - gt[:, None, :, :]).sum() / (2 * pred.shape[0]) + 1e-4 * MSE
     # return MSE + MAE
     # return torch.square(pred - gt[:, None, :, :]).mean()
     # return MSE * 1e-6 + MAE * 1e-6

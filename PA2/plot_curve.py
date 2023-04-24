70a7410fc616939c6a8ae72305679525f694ba51
libn <libn@shanghaitech.edu.cn>
2023-04-24 11:08:26 +0800

    add result plotting function

--- PA2/plot_curve.py
+++ PA2/plot_curve.py
@@ -3,8 +3,7 @@ from matplotlib import pyplot as plt
 heads = [1, 3, 9, 11]
 layers = [1, 2, 4, 6, 8, 10, 12]
 
-fig1 = plt.figure(figsize=(20, 20))
-fig2 = plt.figure(figsize=(20, 20))
+plt.figure(figsize=(20, 10))
 
 for head in heads:
     MAE = []
@@ -16,9 +15,10 @@ for head in heads:
             obo = float(content[1].split(":")[1])
             MAE.append(mae)
             OBO.append(obo)
-    fig1.plot(layers, MAE, label=f'num_head={head}')
-    fig1.legend()
-    fig2.plot(layers, OBO, label=f'num_head={head}')
-    fig2.legend()
-fig1.savefig("MAE_curve.png")
-fig2.savefig("OBO_curve.png")
+    plt.subplot(1, 2, 1)
+    plt.plot(layers, MAE, label=f'num_head={head}')
+    plt.legend()
+    plt.subplot(1, 2, 2)
+    plt.plot(layers, OBO, label=f'num_head={head}')
+    plt.legend()
+plt.savefig("MAE&OBO_curve.png")

8f359fd66e118f5d21a7dd584a96b557a0abb971
libn <libn@shanghaitech.edu.cn>
2023-04-25 15:08:38 +0800

    start report

--- PA2/plot_curve.py
+++ PA2/plot_curve.py
@@ -3,11 +3,12 @@ from matplotlib import pyplot as plt
 heads = [1, 3, 9, 11, 33]
 layers = [1, 2, 4, 6, 8, 10, 12]
 
-plt.figure(figsize=(10, 20))
+plt.figure(figsize=(7, 7))
 
 for head in heads:
     MAE = []
     OBO = []
+    out = ''
     for layer in layers:
         with open(f"saved_models/head_{head}_layer_{layer}/test_result.txt", "r") as f:
             content = f.read().split(",")
@@ -15,16 +16,22 @@ for head in heads:
             obo = float(content[1].split(":")[1])
             MAE.append(mae)
             OBO.append(obo)
+            out += "& {:.3f}".format(obo) 
 
-    print(f'heads:{head}\n MAE: {MAE}\n OBO: {OBO}')
-    plt.subplot(2, 1, 1)
-    plt.plot(layers, MAE, label=f'num_head={head}')
-    plt.legend()
-    plt.title("MAE")
-    plt.xlim(0, 12)
-    plt.subplot(2, 1, 2)
-    plt.plot(layers, OBO, label=f'num_head={head}')
+    print(f'{out}')
+    # plt.subplot(2, 1, 1)
+    # plt.plot(layers, MAE, label=f'num_head={head}')
+    # plt.legend()
+    # plt.title("MAE")
+    # plt.xlim(0, 12)
+    # plt.subplot(2, 1, 2)
+    # plt.plot(layers, OBO, label=f'num_head={head}')
+    # plt.legend()
+    # plt.title("OBO")
+    # plt.xlim(0, 12)
+    plt.plot(layers, OBO, label=f"num_head={head}")
     plt.legend()
     plt.title("OBO")
     plt.xlim(0, 12)
-plt.savefig("MAE&OBO_curve.png")
+    plt.xlabel("num_layers")
+plt.savefig("report/img/OBO_curve.png", bbox_inches='tight', pad_inches=0.0)
